cf1365b6562791838419f1589507fcf79b0d9568
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-25 00:04:59 -0400

    Add capability for class-wide facts

--- prescient.rb
+++ prescient.rb
@@ -17,17 +17,31 @@ module Prescient
   
   class Event
     
-    attr_reader :facts
-    
     def initialize
+      # Initialize facts and gather class facts into instance facts
       @facts = {}
+      self.class.facts.each_pair do |sym, fact|
+        @facts[sym] = fact.bind(self) rescue NoMethodError
+      end if self.class.facts
+      
+      # Run the initialize block if given
       yield self if block_given?
     end
     
+    # Create an instance-level fact - applies only to the instance
     def fact(sym, &block)
       raise ArgumentError, "sym argument can't be nil" if sym.nil?
       @facts[sym] = (block or method(sym))
     end
+    attr_reader :facts
+    
+    # Create a class-level fact - applies to all future instances of the class
+    def self.fact(sym, &block)
+      raise ArgumentError, "sym argument can't be nil" if sym.nil?
+      @facts ||= {}
+      @facts[sym] = (block or instance_method(sym))
+    end
+    class << self; attr_reader :facts; end
     
   end
   
--- sandbox.rb
+++ sandbox.rb
@@ -4,21 +4,24 @@ include Prescient
 
 require 'pry'
 
+class MyEvent < Event
+  fact def youth
+    (100 - facts[:age].call) / 100.0
+  end
+end
 
 Studier.new.study [
   
-  Event.new { |e|
+  MyEvent.new { |e|
     e.fact(:age) { 52 }
-    e.fact(:youth) { (100 - e.facts[:age].call) / 100.0 }
+  },
+  
+  MyEvent.new { |e|
+    e.fact(:age) { 2 }
   },
   
   Event.new { |e|
-    e.fact(:age) { 52 }
-    e.fact def e.youth
-      (100 - facts[:age].call) / 100.0
-    end
-    
-    e.fact nil
+    e.fact(:age) { 23 }
   }
   
 ]
